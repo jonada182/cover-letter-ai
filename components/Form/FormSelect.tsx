0d1e5d8d668de912f6f3775d1e3d1bc8f47bec7e
Jonatan Caraballo <jonada182@hotmail.com>
2023-10-05 01:21:22 -0400

    Lower font size for form inputs

--- components/Form/FormSelect.tsx
+++ components/Form/FormSelect.tsx
@@ -1,35 +1,44 @@
-import React, { memo } from "react"
+import React, { memo } from "react";
 
 type Props = {
-  name: string
-  labelName?: string | undefined
-  value: string | number | undefined
-  handleOnChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
-  required?: boolean | undefined
-  options: Array<any>
-}
+  name: string;
+  labelName?: string | undefined;
+  value: string | number | undefined;
+  handleOnChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  required?: boolean | undefined;
+  options: Array<any>;
+};
 
 const FormSelect = (props: Props) => {
   return (
-    <div className="flex flex-col max-w-lg justify-stretch mb-4">
-      {props.labelName && <label className="font-semibold flex justify-between" htmlFor={props.name}>
-        {props.labelName}
-        {props.required && <span className="text-xs text-pink-700 font-normal">Required</span>}
-      </label>}
+    <div className="flex flex-col max-w-lg justify-stretch mb-4 text-sm">
+      {props.labelName && (
+        <label
+          className="font-semibold flex justify-between"
+          htmlFor={props.name}
+        >
+          {props.labelName}
+          {props.required && (
+            <span className="text-xs text-pink-700 font-normal">Required</span>
+          )}
+        </label>
+      )}
       <select
         name={props.name}
         value={props.value}
         onChange={props.handleOnChange}
-        className="cursor-pointer p-3 my-3 rounded outline-none border border-gray-200 bg-white focus:border-pink-600"
+        className="cursor-pointer p-3 my-3 rounded outline-none border border-gray-200 bg-white focus:border-pink-600 text-sm"
       >
         {props.options.map(([key, value]) => {
           return (
-            <option key={value} value={value}>{key}</option>
-          )
+            <option key={value} value={value}>
+              {key}
+            </option>
+          );
         })}
       </select>
     </div>
-  )
-}
+  );
+};
 
-export default memo(FormSelect)
+export default memo(FormSelect);
--- components/Form/FormTextarea.tsx
+++ components/Form/FormTextarea.tsx
@@ -1,20 +1,26 @@
-import React, { memo } from "react"
+import React, { memo } from "react";
 
 type Props = {
-  name: string
-  labelName?: string | undefined
-  placeholder?: string | undefined
-  value: string | number | readonly string[] | undefined
-  large?: boolean
-  handleOnChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
-}
+  name: string;
+  labelName?: string | undefined;
+  placeholder?: string | undefined;
+  value: string | number | readonly string[] | undefined;
+  large?: boolean;
+  handleOnChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+};
 
 const FormTextarea = (props: Props) => {
   return (
-    <div className="flex flex-col max-w-lg justify-stretch mb-4">
-      {props.labelName && <label className="font-semibold" htmlFor={props.name}>{props.labelName}</label>}
+    <div className="flex flex-col max-w-lg justify-stretch mb-4 text-sm">
+      {props.labelName && (
+        <label className="font-semibold" htmlFor={props.name}>
+          {props.labelName}
+        </label>
+      )}
       <textarea
-        className={`flex-grow p-3 transition-all shadow-sm my-3 rounded border border-gray-200 focus:border-pink-600 outline-none ${props.large ? "h-64" : "h-36"} max-h-64`}
+        className={`flex-grow p-3 text-sm transition-all shadow-sm my-3 rounded border border-gray-200 focus:border-pink-600 outline-none ${
+          props.large ? "h-64" : "h-36"
+        } max-h-64`}
         name={props.name}
         id={props.name}
         placeholder={props?.placeholder}
@@ -22,7 +28,7 @@ const FormTextarea = (props: Props) => {
         value={props.value ? props.value : ""}
       />
     </div>
-  )
-}
+  );
+};
 
-export default memo(FormTextarea)
+export default memo(FormTextarea);
